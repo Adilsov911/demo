d2133b01f6e82a4844ef2c71eb0b3ae5c5c45c74
Adilsov911 <adiles@code.edu.az>
2022-08-02 03:07:40 +0400

    New app owner controller ready

---
+++ AptekMenage/Program.cs
@@ -0,0 +1,97 @@
+﻿using AptekMenage.Controllers;
+using Core.Constants;
+using Core.Helpers;
+using System;
+
+namespace AptekMenage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            OwnerController ownerController = new OwnerController();
+            AdminController admincontroller = new AdminController();
+            var admin = admincontroller.Authenticade();
+
+            if (admin != null)
+            {
+                Helper.WriteTextWithColor(ConsoleColor.Green, $"Welcome, {admin.Username}");
+                Helper.WriteTextWithColor(ConsoleColor.White, "------");
+
+                while (true)
+                {
+                    Helper.WriteTextWithColor(ConsoleColor.Blue, "Main Menu:");
+                    Helper.WriteTextWithColor(ConsoleColor.Cyan, "Owner Menu - 1");
+                    
+
+
+                    Console.WriteLine("--------------------------------------------------");
+
+                    Helper.WriteTextWithColor(ConsoleColor.Magenta, "Select Options:");
+                    string number = Console.ReadLine();
+                    int selectedNumber;
+                    bool result = int.TryParse(number, out selectedNumber);
+
+
+                    if (result)
+                    {
+                        if (selectedNumber == 1)
+                        {
+                            Helper.WriteTextWithColor(ConsoleColor.Yellow, "1 - Create Owner");
+                            Helper.WriteTextWithColor(ConsoleColor.Yellow, "2 - Update Owner");
+                            Helper.WriteTextWithColor(ConsoleColor.Yellow, "3 - GetAll Owner");
+                            Helper.WriteTextWithColor(ConsoleColor.Yellow, "4 - Delete Owner");
+                            Helper.WriteTextWithColor(ConsoleColor.Magenta, "Select Options:");
+                            number = Console.ReadLine();
+
+
+                            result = int.TryParse(number, out selectedNumber);
+                            if (selectedNumber >= 0 && selectedNumber <= 6)
+                            {
+                                switch (selectedNumber)
+                                {
+
+                                    case (int)OwnerOptions.OwnerCreat:
+                                        ownerController.Creat();
+                                        break;
+                                    case (int)OwnerOptions.UpdateOwner:
+                                        ownerController.Update();
+                                        break;
+                                    case (int)OwnerOptions.GetAllOwner:
+                                        ownerController.GetAll();
+                                        break;
+                                    case (int)OwnerOptions.DeleteOwner:
+                                        ownerController.Delete();
+                                        break;
+                                    case (int)OwnerOptions.Exit:
+                                        ownerController.Exit();
+                                        break;
+
+
+                                }
+                            }
+                            else
+                            {
+                                Helper.WriteTextWithColor(ConsoleColor.Red, "Please enter correct number");
+                            }
+                        }
+                        else
+                        {
+                            Helper.WriteTextWithColor(ConsoleColor.Red, "Please, Select Correct Options...");
+                        }
+                    }
+                    else
+                    {
+                        Helper.WriteTextWithColor(ConsoleColor.Red, "Please, Select Correct Options...");
+                    }
+
+                }
+            }
+            else
+            {
+                Helper.WriteTextWithColor(ConsoleColor.Red, "Username or Password incorrect");
+                
+            }
+        }
+    }
+}
---
+++ DataAcces/Repositories/Implementations/DrugRepository.cs
@@ -0,0 +1,116 @@
+﻿using Core.Entities;
+using DataAcces.Context;
+using DataAcces.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Repositories.Implementations
+{
+    public class DrugRepository : IRepository<Drug>
+    {
+        private static int id;
+        public Drug Create(Drug entity)
+        {
+            id++;
+            entity.Id = id;
+            try
+            {
+                DbContext.Druggis.Add(entity);
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return entity;
+        }
+
+        public void Delete(Drug entity)
+        {
+            try
+            {
+                DbContext.Druggis.Remove(entity);
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+
+
+            }
+        }
+
+        public Drug Get(Predicate<Drug> filter = null)
+        {
+            try
+            {
+
+                if (filter == null)
+                {
+                    return DbContext.Druggis[0];
+                }
+                else
+                {
+                    return DbContext.Druggis.Find(filter);
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public List<Drug> GetAll(Predicate<Drug> filter = null)
+        {
+            try
+            {
+
+                if (filter == null)
+                {
+                    return DbContext.Druggis;
+                }
+                else
+                {
+                    return DbContext.Druggis.FindAll(filter);
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public void Update(Drug entity)
+        {
+            try
+            {
+
+                var drug = DbContext.Druggis.Find(g => g.Id == entity.Id);
+                if (drug != null)
+                {
+                    drug.Price = entity.Price;
+                    drug.Count = entity.Count;
+                    drug.Name = entity.Name;
+
+
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
